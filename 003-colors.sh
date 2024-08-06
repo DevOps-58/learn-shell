b8372f23d6726f5dc832c83ece2f1595e3372b89
Girisha97 <girishavanaparthi02@gmail.com>
2024-08-06 15:45:33 -0500

    Auto Push

--- 003-colors.sh
+++ 003-colors.sh
@@ -6,4 +6,4 @@ echo -e "\e[32m This shows Green color \e[0m"
 
 echo -e "\e[31m Red color \e[0m"
 echo -e "\e[33m This is yellow color \e[0m"
-echo -e "\e[36m This shows magenta color \e[0m"
+echo -e "\e[36m This shows blue color \e[0m"
