11d1cc203f1ebee818e4912ae84256435216928a
Girisha97 <girishavanaparthi02@gmail.com>
2024-08-06 15:31:20 -0500

    Auto Push

--- 002-escSeqCharacters.sh
+++ 002-escSeqCharacters.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo -e "Line1 \n Line2"  # "-e  To Enable escSeqCharacters"
+echo -e "Line1\nLine2"  # "-e  To Enable escSeqCharacters"
                 #\n - New Line
+
+                
 echo -e "Line3 \t Line4" # \t - Move the cursor to a tab space
 
 
