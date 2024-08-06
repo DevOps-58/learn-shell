04d04195c0de3ed699f10344b381716385a578b2
Girisha97 <girishavanaparthi02@gmail.com>
2024-08-06 15:54:29 -0500

    Auto Push

--- 003-colors.sh
+++ 003-colors.sh
@@ -13,3 +13,7 @@ echo -e "\e[34m This shows Blue color \e[0m"
 echo -e "\e[36m This shows Cyan color \e[0m"
 
 echo -e "\e[35m This shows Magenta color \e[0m"
+
+#To change background color
+
+echo -e "\e[41;34m I am printing text with background color \e[0m"
