964f3773b9d7f36f79a3b6cfa58e3b1fc529f037
Girisha97 <girishavanaparthi02@gmail.com>
2024-08-07 13:24:54 -0500

    Auto Push

---
+++ shell-scripting/010-functions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# How to declare a function
+
+func() {
+    echo "Devops"
+    echo "Java"
+    echo "Python"
+    who
+    uptime
+    df -h
+}
+
+echo "Today we are learning Bash"
+
+func
