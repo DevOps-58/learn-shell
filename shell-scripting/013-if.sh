1166aca4a9a9ec8c3e5a30ab3822910787cfeb65
Girisha97 <girishavanaparthi02@gmail.com>
2024-08-07 14:16:00 -0500

    Auto Push

--- shell-scripting/013-if.sh
+++ shell-scripting/013-if.sh
@@ -9,10 +9,27 @@ ACTION=$1
 #  exit 0
 #fi 
 
-if [ "$ACTION" = "start" ] ; then
-   echo -e "\e[32m Backend is starting \e[0m"
-   exit 0
-else
-   echo -e "\e[31m start is valid option \e[0m"
-   exit 3
-fi
+#if [ "$ACTION" = "start" ] ; then
+#   echo -e "\e[32m Backend is starting \e[0m"
+#   exit 0
+#else
+#   echo -e "\e[31m start is valid option \e[0m"
+#   exit 3
+#fi
+
+# Demo on else 
+if [ "$ACTION" = "start" ] ; then 
+    echo -e "\e[32m Backed is starting \e[0m"
+    exit 0
+
+elif  [ "$ACTION" = "stop" ] ; then 
+    echo -e "\e[31m Backed is stopping \e[0m"
+    exit 1
+
+elif  [ "$ACTION" = "restart" ] ; then 
+    echo -e "\e[31m Backed is restarting \e[0m"
+    exit 2
+else 
+    echo -e "\e[35m Valid options are start stop restart only \e[0m"
+    exit 3 
+fi  
