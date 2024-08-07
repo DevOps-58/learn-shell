3ef57f0fc49921cdcb5046b18781a436c74ac01a
Girisha97 <girishavanaparthi02@gmail.com>
2024-08-07 14:06:05 -0500

    Auto Push

--- shell-scripting/013-if.sh
+++ shell-scripting/013-if.sh
@@ -5,6 +5,6 @@ echo -e "Demo on if condition"
 ACTION=$1
 
 if [ "$ACTION" = "start" ] ; then 
-    echo -e "\e[32m Backed is starting \e[0m"
+    echo -e "\e[32m Backend is starting \e[0m"
     exit 0
 fi  
